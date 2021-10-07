3f2fc0c49da41c313ce4fd877900aadacc965a01
horsemaker <yash_ghodekar@yahoo.in>
2021-10-07 16:33:14 +0530

    feat: comparing total power

--- 17. Name Power & Power/index.js
+++ 17. Name Power & Power/index.js
@@ -18,6 +18,7 @@ function checkPowers(personOne, personTwo) {
   namePowerPersonTwo = personTwo.name.length * 35
   // console.log(namePowerPersonTwo)
 
+  // Checking Name Power
   if (namePowerPersonOne > namePowerPersonTwo) {
     console.log(`${personOne.name} has more name power than ${personTwo.name}.`)
   } else if (namePowerPersonOne < namePowerPersonTwo) {
@@ -28,12 +29,30 @@ function checkPowers(personOne, personTwo) {
 
   console.log('---------------------------------------')
 
+  // Checking Power
   if (personOne.power > personTwo.power) {
     console.log(`${personOne.name} has more power than ${personTwo.name}.`)
   } else if (personOne.power < personTwo.power) {
     console.log(`${personTwo.name} has more power than ${personOne.name}.`)
   } else {
     console.log(`${personOne.name} & ${personTwo.name} have equal powers.`)
+  }
+
+  totalPowerOne = namePowerPersonOne + personOne.power
+  totalPowerTwo = namePowerPersonTwo + personTwo.power
+
+  if (totalPowerOne > totalPowerTwo) {
+    console.log(
+      `${personOne.name} has more total power than ${personTwo.name}.`
+    )
+  } else if (totalPowerOne < totalPowerTwo) {
+    console.log(
+      `${personTwo.name} has more total power than ${personOne.name}.`
+    )
+  } else {
+    console.log(
+      `${personOne.name} & ${personTwo.name} have equal total powers.`
+    )
   }
 }
 
