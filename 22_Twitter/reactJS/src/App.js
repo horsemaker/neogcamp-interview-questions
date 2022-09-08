54b1838668523872483e8a2ba7a9a4374d691c89
Aritra_Chowdhury <74394757+Aritra-art@users.noreply.github.com>
2022-09-08 23:11:26 +0530

    App.js Modified
    The code can be done in a much simpler and easier way. If you like my approach, please accept my PR :)

--- 22_Twitter/reactJS/src/App.js
+++ 22_Twitter/reactJS/src/App.js
@@ -1,66 +1,54 @@
 import "./styles.css";
+import { useState } from "react";
 
-import React, { useState } from "react";
-var counter = 30;
-var store = [];
-var p = 0;
 export default function App() {
-  var myStyle = {
-    color: "black"
+  let counter = 30;
+  const [value, setValue] = useState("");
+  const [color, setColor] = useState("black");
+  const [btnValue, setBtnValue] = useState(false);
+  const [display, setDisplay] = useState(null);
+
+  const inputHandler = (e) => {
+    setValue(e.target.value);
+    counter = counter - value.length;
+    console.log(counter);
+    if (counter <= 15 && counter >= 0) {
+      setColor("yellow");
+      setBtnValue(false);
+    } else if (counter < 0) {
+      setColor("red");
+      setBtnValue(true);
+    }
   };
-  const [input_text, set_input_text] = useState(myStyle);
-  var [text, set_text] = useState("");
-  const [content, set_content] = useState("");
-  const [value, set_value] = useState(true);
-
-  function count(txt) {
-    text = txt;
-    set_text(text);
-    counter = 30 - text.length;
-
-    if (counter === 15) {
-      myStyle = { color: "yellow" };
-      set_input_text(myStyle);
-    } else if (counter <= 0) {
-      myStyle = { color: "red" };
-      set_input_text(myStyle);
-    } else if (counter > 15) {
-      myStyle = { color: "black" };
-      set_input_text(myStyle);
-    }
-    if (counter <= 0 || counter === 30) {
-      set_value(true);
-    } else {
-      set_value(false);
-    }
-  }
-
-  function text_Content() {
-    store[p] = text;
-    p++;
-    var x = content;
-    console.log(store);
-    for (var i = 0; i < store.length; i++) {
-      set_content(x + "\n" + store[i]);
-    }
-  }
 
   return (
     <div className="App">
-      <h1>Twitter</h1>
       <textarea
-        rows="5"
-        cols="50"
-        placeholder="Enter something here..."
-        onInput={(event) => count(event.target.value)}
-      ></textarea>
-      <button disabled={value} onClick={() => text_Content()}>
-        Submit
+        placeholder="enter your text"
+        onInput={inputHandler}
+        style={{
+          color: color,
+          height: "8rem",
+          padding: "1rem",
+          fontSize: "1rem",
+          margin: "2rem",
+          outline: "none"
+        }}
+      />
+      <button
+        style={{
+          width: "40%",
+          margin: "auto",
+          padding: "1rem"
+        }}
+        disabled={btnValue}
+        onClick={() => {
+          setDisplay(value);
+        }}
+      >
+        SUBMIT
       </button>
-      <div>
-        Characters: <label style={input_text}>{counter}</label>
-      </div>
-      <div className="output">{content}</div>
+      <p>{display}</p>
     </div>
   );
 }
